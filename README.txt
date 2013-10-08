d38a15d561dff4ff967b2379a1bf0f923c198fe3
Bill Bushey <wbushey@gmail.com>
2013-10-08 16:45:05 -0500

    Imported the group/changeproperties.html page from gs.skin.ogn.edem

---
+++ README.txt
@@ -0,0 +1,33 @@
+==============================
+``edem.group.properties``
+==============================
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Customized version of the Change Group Properties page
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Author: `Bill Bushey`_
+:Contact: Bill Bushey <bill.bushey@e-democracy.org>
+:Date: 2013-10-08
+:Organization: `E-Democracy.org`_
+:Copyright: This document is licensed under a
+  `Creative Commons Attribution-Share Alike 3.0 License`_
+  by `E-Democracy.org`_.
+
+Introduction
+===========
+
+This egg customizes the Change Group Properties page provided by
+gs.group.properties. Currently, this egg adds several fields to the page.
+
+Resources
+=========
+
+- Code repository: https://github.com/e-democracy/edem.group.properties 
+- Questions and comments to http://groupserver.org/groups/development
+- Report bugs at https://redmine.iopen.net/projects/groupserver
+
+.. _GroupServer: http://groupserver.org/
+.. _E-Democracy.org: http://e-democracy.org/
+.. _Bill Bushey: http://groupserver.org/p/wbushey
+.. _Creative Commons Attribution-Share Alike 3.0 License:
+   http://creativecommons.org/licenses/by-sa/3.0/
---
+++ edem/group/properties/change.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+from gs.content.form.utils import enforce_schema
+from gs.group.privacy.visibility import GroupVisibility
+from gs.group.base.form import GroupForm
+from interfaces import IEDemGroupProperties
+from gs.group.properties.change import ChangePropertiesForm
+from gs.content.form.utils import enforce_schema
+from gs.content.form.disabledtextwidget import disabled_text_widget
+from zope.formlib import form
+
+class EDemChangePropertiesForm(ChangePropertiesForm):
+    def __init__(self, context, request):
+        ChangePropertiesForm.__init__(self, context, request)
+        enforce_schema(self.context, IEDemGroupProperties)
+        self.groupVisibility = GroupVisibility(self.groupInfo)
+        self.__form_fields = None
+        self.label = u'Change Group Properties'
+        
+    @property
+    def form_fields(self):
+        if self.__form_fields == None:
+            form_fields = \
+              form.Fields(IEDemGroupProperties, render_context=True)
+            if self.groupVisibility.isPublic:
+                form_fields['mshipCriterion'].custom_widget = \
+                  disabled_text_widget
+            self.__form_fields = form_fields
+        return self.__form_fields
