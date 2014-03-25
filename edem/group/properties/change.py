a58b284a762d898601e741de9ac6c8c8110f8b3b
William Bushey <wbushey@gmail.com>
2014-03-25 15:49:26 -0500

    Made change.py PEP 8 and Python 3 ready

--- edem/group/properties/change.py
+++ edem/group/properties/change.py
@@ -1,12 +1,12 @@
 # coding=utf-8
-from gs.content.form.utils import enforce_schema
+from __future__ import absolute_import, unicode_literals
 from gs.group.privacy.visibility import GroupVisibility
-from gs.group.base.form import GroupForm
 from gs.skin.ogn.edem.interfaces import IEDemGroupProperties
 from gs.group.properties.change import ChangePropertiesForm
 from gs.content.form.utils import enforce_schema
 from gs.content.form.disabledtextwidget import disabled_text_widget
 from zope.formlib import form
+
 
 class EDemChangePropertiesForm(ChangePropertiesForm):
     def __init__(self, context, request):
@@ -15,14 +15,14 @@ class EDemChangePropertiesForm(ChangePropertiesForm):
         self.groupVisibility = GroupVisibility(self.groupInfo)
         self.__form_fields = None
         self.label = u'Change Group Properties'
-        
+
     @property
     def form_fields(self):
-        if self.__form_fields == None:
+        if self.__form_fields is None:
             form_fields = \
-              form.Fields(IEDemGroupProperties, render_context=True)
+                form.Fields(IEDemGroupProperties, render_context=True)
             if self.groupVisibility.isPublic:
                 form_fields['mshipCriterion'].custom_widget = \
-                  disabled_text_widget
+                    disabled_text_widget
             self.__form_fields = form_fields
         return self.__form_fields
